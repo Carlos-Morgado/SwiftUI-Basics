fe1ec4d52be8688383b1fd41e42ad2422485e085
Carlos Morgado <cmorgadoalarcon@gmail.com>
2023-06-22 10:26:46 +0200

    Initializing SwiftUI-Basics Project

---
+++ SwiftUI-Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI-Basics
+//
+//  Created by Carlos Morgado on 22/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
